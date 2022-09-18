4117576d6b84ffb6c11393ef4c68409d49469e0c
maximecib56 <maximecib56@gmail.com>
2022-09-18 17:34:52 +0300

    creation of class Item in Models folder

---
+++ AppName/AppName/AppName.Shared/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿
+public class Item
+{
+    public int Id { get; set; }
+    
+    public string name;
+
+    public float price;
+
+    public TagType tag;
+
+
+}
+
+public enum TagType
+{
+    Food,
+    Cleaning_Stuff,
+    Entertainment
+}
