79451b561d279c4b0b4016a451ef73bca970c822
OverealDev <107404704+OverealDev@users.noreply.github.com>
2022-09-27 13:52:56 +0300

    Add class Current_balance

---
+++ reactproject1/reactproject1/src/components/Current_balance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+import './ExpensesList.js';
+
+
+
+public class Current_balance
+{
+	public double totalAmount = 0;
+	public Current_balance()
+	{
+		for (expenses in expensesList)
+		{
+            this.totalAmount += expenses
+        }
+	}
+}
+
+
+
