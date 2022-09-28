0e1f921f8549b6e28c3859ff5e12e781d06eb708
maximecib56 <maximecib56@gmail.com>
2022-09-28 21:17:39 +0300

    Get request to API (conversion from json to item list doesn't work yet)

--- reactproject1/WebApplication2/Controllers/ItemController.cs
+++ reactproject1/WebApplication2/Controllers/ItemController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.IO;
+using System.Text.Json;
 
 namespace WebApplication2.Controllers
 {
@@ -7,5 +10,44 @@ namespace WebApplication2.Controllers
     [ApiController]
     public class ItemController : ControllerBase
     {
+        public Item[] content;
+
+        public ItemController()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "\\expenses.txt");
+
+            StreamReader sr = new("C:\\Users\\Maxime\\Desktop\\S5\\software_engineering_I_II\\3MTech\\reactproject1\\WebApplication2\\Controllers\\expenses.txt");
+
+            var tmp = sr.ReadLine();
+
+            var ItemList = JsonSerializer.Deserialize<IList<Item>>(tmp);
+
+            Item[] tmpContent = Array.Empty<Item>();
+
+            foreach(var item in ItemList)
+            {
+                tmpContent = (Item[])tmpContent.Append(item);
+            }
+
+            content = tmpContent;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return content;
+        }
+        /*
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+        }*/
     }
 }
