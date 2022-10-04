ccca297204faeb980e10dce0193099b3a0d82fc5
maximecib56 <maximecib56@gmail.com>
2022-10-04 20:58:34 +0300

    Fetch request coded, API works but fetch doesn't

--- reactproject1/reactproject1/src/App.js
+++ reactproject1/reactproject1/src/App.js
@@ -1,15 +1,42 @@
 import ExpenseForm from "./components/ExpenseForm"
 import Expenses from "./components/Expenses"
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 function App() {
-    const [expenses, setExpenses] = useState([])
+    const [expenses, setExpenses] = useState("")
 
     function addExpenseHandler(expense) {
         setExpenses((prevExpenses) => {
             return [expense, ...prevExpenses];
         });
     }
+
+    const fetchExpensesHandler = useCallback(async () => {
+        try {
+            const response = await fetch('https://localhost:7267/api/Item');
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const data = await response.json();
+
+            const transformedExpenses = data.map((expenseData) => {
+                return {
+                    id: expenseData.id,
+                    title: expenseData.title,
+                    amount: expenseData.amount,
+                    date: expenseData.date,
+                };
+            });
+            setExpenses(transformedExpenses);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchExpensesHandler();
+    }, []);
 
     return (
         <div>
