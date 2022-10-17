990642a5782ff059de40ce5f9803cadf1e3e6399
OverealDev <107404704+OverealDev@users.noreply.github.com>
2022-10-18 00:48:25 +0300

    update CurrentBalance, and creating LINQ associated

---
+++ reactproject1/WebApplication2/Models/CurrentBalance.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication2.Models;
+
+
+
+public class Current_balance {
+    public float totalAmount { get; set; }
+    public String currency { get; set; } //Dollar, Euro, ...
+    public float amountAverage { get; set; }
+
+    public Current_balance(String currency_, List<Item> itemList)
+    {
+        this.currency = currency_;
+
+        foreach (Item item in itemList) {
+
+            this.totalAmount = this.totalAmount + item.Amount;
+        }
+
+    }
+
+}
+   
---
+++ reactproject1/WebApplication2/Models/LINGItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace WebApplication2.Models;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Item item1 = new Item();
+        Item item2 = new Item();
+        Item item3 = new Item();
+
+        item1.Amount = 3;
+        item1.Type = Type.Books;
+
+        item2.Amount = 5;
+        item2.Type = Type.Books;
+
+        item3.Amount = 6;
+        item3.Type = Type.Cleaning_stuff;
+
+
+        List<Item> itemList = new List<Item>{item1, item2, item3};
+        Current_balance current_balance = new Current_balance("Euro", itemList);
+        Console.WriteLine(current_balance.totalAmount);
+
+
+        var PricePerType = from item in itemList
+                           group item by new
+                           {
+                               item.Type
+                           } into rows
+                           select new
+                           {
+                               Average = rows.Average(p => p.Amount),
+                               rows.Key.Type
+                           };
+
+
+        foreach(var test in PricePerType)
+        {
+            Console.WriteLine(test + " ");
+
+        }
+
+
+    }
+}
