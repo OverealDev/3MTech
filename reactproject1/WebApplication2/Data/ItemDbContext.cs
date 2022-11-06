bc9d91def062fafcc87fa6ba8a43f549241db842
maximecib56 <maximecib56@gmail.com>
2022-11-06 14:28:11 +0200

    Database created with migrations

---
+++ reactproject1/WebApplication2/Data/ItemDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class ItemDbContext : DbContext
+    {
+        public ItemDbContext(DbContextOptions<ItemDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
--- reactproject1/WebApplication2/Program.cs
+++ reactproject1/WebApplication2/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +20,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ItemDbContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 
 var app = builder.Build();
