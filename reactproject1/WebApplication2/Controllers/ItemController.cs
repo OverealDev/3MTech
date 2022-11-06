dbc9cbb27dab3d4b5abb15e005755e8256dd8438
maximecib56 <maximecib56@gmail.com>
2022-11-06 17:47:44 +0200

    Get, get by id and post request coded for the database

--- reactproject1/WebApplication2/Controllers/ItemController.cs
+++ reactproject1/WebApplication2/Controllers/ItemController.cs
@@ -4,6 +4,8 @@ using WebApplication2.Models;
 using System.IO;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Cors;
+using WebApplication2.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication2.Controllers
 {
@@ -11,33 +13,39 @@ namespace WebApplication2.Controllers
     [ApiController]
     public class ItemController : ControllerBase
     {
-        public Item[] content;
+        private readonly ItemDbContext _context;
 
-        public ItemController()
+        public ItemController(ItemDbContext context)
         {
-            var path = @".\Controllers\expenses.txt";
-
-            using (StreamReader sr = new(path))
-            {
-                var tmp = sr.ReadToEnd();
-
-                content = JsonConvert.DeserializeObject<Item[]>(tmp);
-            } 
-            
+            _context = context;
         } 
 
         [HttpGet]
-        public IEnumerable<Item> Get()
+        public async Task<IEnumerable<Item>> Get()
         {
-            return content;
+            return await _context.Items.ToListAsync();
         }
-        /*
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Item), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            return item == null ? NotFound() : Ok(item);
+        }
+        
         [HttpPost]
-        public IActionResult Create(Item item)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Item item)
         {
+            await _context.Items.AddAsync(item);
+            await _context.SaveChangesAsync();
 
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
         }
 
+        /*
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
