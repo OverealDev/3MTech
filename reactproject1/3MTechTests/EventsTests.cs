1302b529a931216ded2e12ffb7d8892215e392b1
OverealDev <107404704+OverealDev@users.noreply.github.com>
2022-11-18 23:20:48 +0200

    event test (not functional)

---
+++ reactproject1/3MTechTests/EventsTests.cs
@@ -0,0 +1,21 @@
+﻿
+using WebApplication2.Events;
+
+namespace _3MTechTests
+{
+    [TestClass]
+    public class EventsTests
+    {
+        [TestMethod]
+        public void TestEvents_CheckingIfOverpriced()
+        {
+            // Arrange
+            decimal amountAbove = 100_000_000_000;
+            decimal amountBelow = 10;
+            Events events = new Events();   
+
+            // Act and assert
+            Assert.ThrowsException<WebApplication2.Events.OverpricedEventArgs>(() => events.CheckingIfOverpriced(amountAbove));
+        }
+    }
+}
