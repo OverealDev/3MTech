7177bdb271687c76eb27e88d09270e8a34f0e7e5
OverealDev <107404704+OverealDev@users.noreply.github.com>
2022-11-21 18:38:30 +0200

    unit test LINQ (non-fuctional yet)

---
+++ reactproject1/3MTechTests/LINQtests.cs
@@ -0,0 +1,57 @@
+﻿using WebApplication2.Controllers;
+using WebApplication2.Models;
+
+
+
+namespace _3MTechTests
+{
+    [TestClass]
+    public class LINQtests
+    {
+        [TestMethod]
+        public void LINQtestsMethod() {
+
+
+            Item item1 = new Item();
+            Item item2 = new Item();
+            Item item3 = new Item();
+            Item item4 = new Item();
+
+            item1.Amount = 3;
+            item1.Type = WebApplication2.Models.Type.Books;
+
+            item2.Amount = 5;
+            item2.Type = WebApplication2.Models.Type.Books;
+
+            item3.Amount = 2;
+            item3.Type = WebApplication2.Models.Type.Books;
+
+            item4.Amount = 6;
+            item4.Type = WebApplication2.Models.Type.Cleaning_stuff;
+
+            LINQRequests requests = new LINQRequests();
+            IQueryable<PriceAndType> results = requests.LINQFunction(item1, item2, item3, item4);
+
+            bool flag = true;
+            int compter = -1;
+            List<float> TrueResult = new List<float>();
+            TrueResult.Add(10 / 3);
+            TrueResult.Add(6);
+
+            foreach (PriceAndType tests in results)
+            {
+
+                compter = compter + 1;
+                if ((tests.Average) != TrueResult[compter])
+                {
+                    flag = false;
+                }
+
+            }
+
+            Assert.IsTrue(flag);
+
+
+        }
+    }
+}
--- reactproject1/WebApplication2/Models/Item.cs
+++ reactproject1/WebApplication2/Models/Item.cs
@@ -29,10 +29,48 @@ namespace WebApplication2.Models
 
         }
 
+
+        
+
     }
 
     public enum Type
     {
         Books, Cleaning_stuff, Food
     }
+
+    public class PriceAndType
+    {
+        public float Average { get; set; }
+        public Type PriceType { get; set; }
+    }
+
+
+    public class LINQRequests
+    {
+
+        public IQueryable<PriceAndType> LINQFunction(Item item1, Item item2, Item item3, Item item4)
+        {
+            List<Item> itemList = new List<Item> { item1, item2, item3, item4 };
+            Current_balance current_balance = new Current_balance("Euro", itemList);
+
+
+
+            var PricePerType_ = from item in itemList
+                                group item by new
+                                {
+                                    item.Type
+                                } into rows
+                                select new
+                                {
+                                    Average = rows.Average(p => p.Amount),
+                                    PriceType = rows.Key.Type
+
+                                };
+
+            return (IQueryable<PriceAndType>)PricePerType_;
+
+        }
+    }
+
 }
