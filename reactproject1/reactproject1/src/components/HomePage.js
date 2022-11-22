c37b867408dcc46daf37f25efaee6f618e4fa986
maximecib56 <maximecib56@gmail.com>
2022-11-22 19:48:40 +0200

    New login page

---
+++ reactproject1/reactproject1/src/components/HomePage.js
@@ -0,0 +1,52 @@
+import { useState, useCallback, useEffect } from 'react'
+import Expenses from "./Expenses"
+import ExpenseForm from "./ExpenseForm"
+
+function HomePage() {
+
+    const [expenses, setExpenses] = useState("")
+
+    function addExpenseHandler(expense) {
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
+    }
+
+    const fetchExpensesHandler = useCallback(async () => {
+        try {
+            const response = await fetch('https://localhost:5001/api/Item');
+            //if (!response.ok) {
+            //    throw new Error('Something went wrong!');
+            //}
+            console.log("1")
+            const data = await response.json();
+            console.log("2")
+            const transformedExpenses = data.map((expenseData) => {
+                return {
+                    id: expenseData.id,
+                    title: expenseData.title,
+                    amount: expenseData.amount,
+                    date: new Date(Date.parse(expenseData.date)),
+                };
+            });
+            console.log(transformedExpenses)
+            setExpenses(transformedExpenses);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, []);
+
+
+    useEffect(() => {
+        fetchExpensesHandler();
+    }, []);
+
+    return (
+        <div>
+            <ExpenseForm onAddExpense={addExpenseHandler} />
+            <Expenses expenses={expenses} />
+        </div>
+    )
+}
+
+export default HomePage
